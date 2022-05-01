444ee0cf6b877b72a89f8aeec130780a74e659e8
Sean Current <seancurrent527@gmail.com>
2022-05-01 12:25:50 -0400

    Fixed filenames for consistency.

--- main.sh
+++ main.sh
@@ -6,6 +6,11 @@ set -e
 #                                            MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt
 
 python make_multiplex.py -N 3000 -m 3 -c 50 -pin 0.95 -pout 0.01 -name MyGraph
-python bespoke-multiplex/label_nodes_multiplex.py MyGraph_edgelist_layer{i}.txt 3 10 MyGraph_labels.txt
-python bespoke-multiplex/run_bespoke_multiplex.py MyGraph_edgelist_layer{i}.txt 3 MyGraph_training.txt 500 \
+
+python bespoke-multiplex-ranked-choice/label_nodes.py MyGraph_edgelist_layer{i}.txt 3 5 MyGraph_labels.txt
+python bespoke-multiplex-ranked-choice/run_bespoke.py MyGraph_edgelist_layer{i}.txt 3 MyGraph_training.txt 500 \
+                                        MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt
+
+python bespoke-main/label_nodes.py MyGraph_edgelist_layer0.txt 5 MyGraph_labels.txt
+python bespoke-main/run_bespoke.py MyGraph_edgelist_layer0.txt MyGraph_training.txt 500 \
                                         MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt
