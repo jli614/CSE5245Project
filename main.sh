5673f63899ceadb6e2b7afb89db71e123ac7b17f
Sean Current <seancurrent527@gmail.com>
2022-05-01 16:05:31 -0400

    Bash file for generating results.

--- main.sh
+++ main.sh
@@ -9,6 +9,9 @@ set -e
 # python bespoke-main/run_bespoke.py ./data/dkpol_edgelist_layer0.txt ./data/dkpol_training.txt 50 \
 #                                         ./data/dkpol_labels.txt ./data/dkpol_bespoke.txt --eval_src ./data/dkpol_communities.txt
 
+outfile="./results.txt"
+tmpfile='./res.tmp'
+
 N=1000
 L=3
 C=50
@@ -17,25 +20,28 @@ Pout=0.01
 Nfeats=4
 Cfind=$(($N / 5))
 
+echo "N $N L $L C $C Pin $Pin Pout $Pout Nfeats $Nfeats" > $outfile
+
 python make_multiplex.py -N $N -m $L -c $C -pin $Pin -pout $Pout -name MyGraph
 
+echo "Ranked Choice" >> $outfile
 python bespoke-multiplex-ranked-choice/label_nodes.py MyGraph_edgelist_layer{i}.txt $L $Nfeats MyGraph_labels.txt
 python bespoke-multiplex-ranked-choice/run_bespoke.py MyGraph_edgelist_layer{i}.txt $L MyGraph_training.txt $Cfind \
-                                        MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt
-
-
+                                        MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt > $tmpfile
+tail -1 $tmpfile >> $outfile
+echo "Weighted" >> $outfile
 python bespoke-multiplex-weighted/combine_graph.py MyGraph_edgelist_layer{i}.txt --layers $L\
                                           --out MyGraph_edgelist_layer_combined.txt
 python bespoke-multiplex-weighted/label_nodes.py MyGraph_edgelist_layer_combined.txt $Nfeats MyGraph_labels.txt
 python bespoke-multiplex-weighted/run_bespoke.py MyGraph_edgelist_layer_combined.txt MyGraph_training.txt $Cfind \
-                                                 MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt
-
-
-
+                                                 MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt  > $tmpfile
+tail -1 $tmpfile >> $outfile
+echo "Layer-wise" >> $outfile
 for ((i=0 ; i<$L ; i++));
 do
     echo $i
     python bespoke-main/label_nodes.py "MyGraph_edgelist_layer${i}.txt" $Nfeats MyGraph_labels.txt
     python bespoke-main/run_bespoke.py "MyGraph_edgelist_layer${i}.txt" MyGraph_training.txt $Cfind \
-        MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt
-done                          
+        MyGraph_labels.txt MyGraph_bespoke.txt --eval_src MyGraph_communities.txt  >> $tmpfile
+    tail -1 $tmpfile >> $outfile
+done
